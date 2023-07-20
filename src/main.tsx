465dea2a0a5422fdbfc7d184385e14096393643e
Archisketch-Ben <ben@archisketch.com>
2023-07-20 16:26:31 +0900

    feat: init

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,10 @@ import './App.css'
 import App from './App'
 import { BrowserRouter, Link, Route, Routes } from 'react-router-dom'
 import { createRoot } from 'react-dom/client'
+import '@fontsource/roboto/300.css'
+import '@fontsource/roboto/400.css'
+import '@fontsource/roboto/500.css'
+import '@fontsource/roboto/700.css'
 
 createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
