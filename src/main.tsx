ee88ab7b5f5195929bce94f5bcb508bf6e5eadfd
Archisketch-Ben <ben@archisketch.com>
2023-07-21 13:54:44 +0900

    feat: preparing for Heroku deployment

--- src/main.tsx
+++ src/main.tsx
@@ -1,29 +1,18 @@
 import React from 'react'
 import './App.css'
 import App from './App'
-import { BrowserRouter, Link, Route, Routes } from 'react-router-dom'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import { createRoot } from 'react-dom/client'
+import '@fontsource/roboto/300.css'
+import '@fontsource/roboto/400.css'
+import '@fontsource/roboto/500.css'
+import '@fontsource/roboto/700.css'
 
 createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<App />} />
-        <Route
-          path="/about"
-          element={
-            <>
-              <div className="text-center">
-                <h1 className="text-xl">About</h1>
-                <div>
-                  <Link to="/" className="text-purple-400 underline">
-                    Home
-                  </Link>
-                </div>
-              </div>
-            </>
-          }
-        />
       </Routes>
     </BrowserRouter>
   </React.StrictMode>
